db3b9a1e6dedc18eefb9e8ad1f408f864e6c7c9f
Gary Peaston <garypeaston@gmail.com>
2019-03-26 08:39:10 +0000

    changed the test js file so it has a popup

--- public/scripts/test.js
+++ public/scripts/test.js
@@ -1,23 +1,34 @@
-const fs = require('fs');
+// import { writeFile } from 'fs';
+// const fs = require('fs');
 
-const data = fs.readFileSync('users.json');
-const logins = JSON.parse(data);
-console.log(logins);
-fs.writeFileSync('users2.JSON', logins);
-
-
+// const data = fs.readFileSync('users.json');
+// const logins = JSON.parse(data);
+// console.log(logins);
+// fs.writeFileSync('users2.JSON', logins);
+// fs.writeFile("temp.txt", userName)
 function signUp() {
-    //reset all fields to blank 
     alert("Well, this did work by alerting the screen");
     var userName = document.getElementById("user_name").value;
     var passWord = document.getElementById("user_pass").value;
     alert(userName);
     alert(passWord);
-    // const data = fs.readFileSync('users.json');
+    // fs.writeFileSync('temp.txt', userName);
+    
+        // if(err) console.log(err);
+        // console.log("success");
+    }
+//     var fs = require('fs');
+//     // var stream = fs.createWriteStream("my_file.txt");
+//     stream.once('open', 'users2.JSON')
+//     stream.write("My first row\n");
+//     stream.write("My second row\n");
+//     stream.end();
+// });
+//     // const data = fs.readFileSync('users.json');
     // const logins = JSON.parse(data);
     // console.log(logins);
     // alert(logins);
-}
+
 
 
 
